d3f10d11eaf4b9f25e351c105243bc4e3ca16326
trevorsmithy <trevor.smithy100@gmail.com>
2026-05-01 17:25:18 -0500

    update to 1.1.7

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker: Easy Mode",
-    ["apkCode"] = 010106,
-    ["verCode"] = 010106,
+    ["apkCode"] = 010107,
+    ["verCode"] = 010107,
 
-    ["appVer"] = "eV1.01.06",
-    ["verStr"] = "1.01.06",
+    ["appVer"] = "eV1.01.07",
+    ["verStr"] = "1.01.07",
 }
