1e3491b11c3d70d00d5f6b68764779486f3aadbe
trevor-smithy <trevor.smithy100@gmail.com>
2026-09-24 02:53:58 -0500

    Update version.lua

--- version.lua
+++ version.lua
@@ -3,6 +3,6 @@ return {
     ["apkCode"] = 010209,
     ["verCode"] = 010209,
 
-    ["appVer"] = "eV1.02.09-pre4",
-    ["verStr"] = "1.02.09-pre4",
+    ["appVer"] = "eV1.02.09-pre3",
+    ["verStr"] = "1.02.09-pre3",
 }
